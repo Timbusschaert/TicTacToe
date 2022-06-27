e5e0f439e403d325fe694bcb9a2f08e738c8f3f5
Busschaert Tim <tim.busschaert@live.be>
2022-06-27 17:57:47 +0200

    mvc

--- Titctactoe-ws/com.paribas.tictactoe/src/main/java/com/paribas/tictactoe/controller/TicTacToeController.java
+++ Titctactoe-ws/com.paribas.tictactoe/src/main/java/com/paribas/tictactoe/controller/TicTacToeController.java
@@ -1,5 +1,33 @@
 package com.paribas.tictactoe.controller;
 
-public class TicTacToeController {
+import java.io.IOException;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+
+public class TicTacToeController {
+	@Controller
+	public class HomeController {
+
+		@RequestMapping(value="/start",method = RequestMethod.POST)
+		@ResponseBody
+		public String startGame(HttpServletResponse response) throws IOException{
+			
+			return "'";
+		}
+		
+		@RequestMapping(value="/play",method = RequestMethod.POST)
+		@ResponseBody
+		public String playGame(HttpServletResponse response) throws IOException{
+			
+			return "'";
+		}
+		
+	}
 }
